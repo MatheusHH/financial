fd9040cad033c937e148d95efca2faefb7ce965f
Matheus Henrique <matheussilva770@gmail.com>
2019-12-11 17:31:16 -0300

    add layout to users, messages and redirects after and before logins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+	layout :layout_by_resource
+
+	private
+
+  	def layout_by_resource
+    	if devise_controller?
+      		"devise_layout"
+    	else
+      		"application"
+    	end
+  	end
+
+  	def after_sign_in_path_for(resource)
+  		home_index_path
+	  end
+
+    def after_sign_out_path_for(resource_or_scope)
+      user_session_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users, :except => [:show]
   get 'home/index'
   root 'home#index'
