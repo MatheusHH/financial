e824a43a8c53ce999f1db802e481d6a178644bef
Matheus Henrique <matheussilva770@gmail.com>
2019-12-11 22:57:06 -0300

    completed layout for supplier, policies and set user

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,10 +1,12 @@
 class SuppliersController < ApplicationController
+  before_action :authenticate_user!
+
   before_action :set_supplier, only: [:show, :edit, :update, :destroy]
 
   # GET /suppliers
   # GET /suppliers.json
   def index
-    @suppliers = Supplier.all
+    @suppliers = policy_scope(Supplier).all
   end
 
   # GET /suppliers/1
@@ -19,16 +21,17 @@ class SuppliersController < ApplicationController
 
   # GET /suppliers/1/edit
   def edit
+    authorize @supplier
   end
 
   # POST /suppliers
   # POST /suppliers.json
   def create
     @supplier = Supplier.new(supplier_params)
-
+    @supplier.user = current_user
     respond_to do |format|
       if @supplier.save
-        format.html { redirect_to @supplier, notice: 'Supplier was successfully created.' }
+        format.html { redirect_to suppliers_url, notice: 'Supplier was successfully created.' }
         format.json { render :show, status: :created, location: @supplier }
       else
         format.html { render :new }
@@ -42,7 +45,7 @@ class SuppliersController < ApplicationController
   def update
     respond_to do |format|
       if @supplier.update(supplier_params)
-        format.html { redirect_to @supplier, notice: 'Supplier was successfully updated.' }
+        format.html { redirect_to suppliers_url, notice: 'Supplier was successfully updated.' }
         format.json { render :show, status: :ok, location: @supplier }
       else
         format.html { render :edit }
@@ -54,6 +57,7 @@ class SuppliersController < ApplicationController
   # DELETE /suppliers/1
   # DELETE /suppliers/1.json
   def destroy
+    authorize @supplier
     @supplier.destroy
     respond_to do |format|
       format.html { redirect_to suppliers_url, notice: 'Supplier was successfully destroyed.' }
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   enum role: [ :admin, :customer ]
+
+  has_many :suppliers, dependent: :destroy
   
 end
