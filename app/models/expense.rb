ed7c75204725a66e58b3ebdc4181e50c41e76773
Matheus Henrique <matheussilva770@gmail.com>
2019-12-18 17:54:56 -0300

    add restore balances

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -9,6 +9,8 @@ class Expense < ApplicationRecord
   enum status: [ :pendente, :pago ]
 
   before_save :update_account_balance, on: [ :create, :update ]
+
+  before_destroy :restore_balance
   #before_update :update_balances
 
 
@@ -29,20 +31,27 @@ class Expense < ApplicationRecord
   	end
   end
 
-  #def update_balances
-  #	sum_income = 0
-  #	incomes = Income.find(user_id: self.user_id)
-  #	incomes.each do |income|
-  #	  sum_income += income.value_cents
-  #	end
-  #	sum_expense = 0
-  #	expenses = Expense.find(user_id: self.user_id)
-  #	expenses.each do |expense|
-  #	  sum_expense += expense.value_cents
-  #	end
-  #	account = Account.find(self.account_id)
-  #	account.balance_cents = sum_income - sum_expense
-  #	self.account.update(balance_cents: account.balance_cents)
-  #end
+  def update_balances
+  	sum_income = 0
+  	incomes = Income.where(user_id: self.user_id).where(account_id: self.account_id)
+  	incomes.each do |income|
+  	  sum_income += income.value_cents
+  	end
+  	sum_expense = 0
+  	expenses = Expense.where(user_id: self.user_id).where(account_id: self.account_id)
+  	expenses.each do |expense|
+  	  sum_expense += expense.value_cents
+  	end
+  	account = Account.find(self.account_id)
+  	account.balance_cents = sum_income - sum_expense
+  	self.account.update(balance_cents: account.balance_cents)
+  end
+
+  def restore_balance
+    account = Account.find(self.account_id)
+    balance_to_be_restored = self.value_cents
+    account.balance_cents += balance_to_be_restored
+    account.update(balance_cents: account.balance_cents) 
+  end
 
 end
