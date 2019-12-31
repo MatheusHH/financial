b7473fb3a2b35c305bd88bc29857f57eb562379c
Matheus Henrique <matheussilva770@gmail.com>
2019-12-30 22:19:18 -0300

    fix expenses, where you can change account moreover value and you will udate balances automaticaly

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -8,15 +8,15 @@ class Expense < ApplicationRecord
 
   enum status: [ :pendente, :pago ]
 
-  after_commit :update_account_balance, on: :create
-  after_commit :update_balances, on: :update 
+  after_commit :update_account_balance_create, on: :create
+  after_commit :update_account_balance_update, on: :update 
 
   before_destroy :restore_balance
 
 
   private
 
-  def update_account_balance
+  def update_account_balance_create
   	if self.account_id.present?
   	  account = Account.find(self.account_id)
   	  if account.balance_cents >= self.value_cents
@@ -31,19 +31,55 @@ class Expense < ApplicationRecord
   	end
   end
 
-  def update_balances
-  	if self.account_id.present?
+  def update_account_balance_update
+    if self.account_id.present?
       account = Account.find(self.account_id)
-      account.balance_cents += self.previous_changes[:value_cents][0]
-      account.update(balance_cents: account.balance_cents)
-      if account.balance_cents >= self.previous_changes[:value_cents][1]
-        account.balance_cents -= self.previous_changes[:value_cents][1]
-        account.update(balance_cents: account.balance_cents) 
+      if self.value_cents_previously_changed? && self.account_id_previously_changed? == false
+        account.balance_cents += self.previous_changes[:value_cents][0]
+        account.update(balance_cents: account.balance_cents)
+        if account.balance_cents >= self.previous_changes[:value_cents][1]
+          account.balance_cents -= self.previous_changes[:value_cents][1]
+          account.update(balance_cents: account.balance_cents) 
+        else
+          if account.balance_cents < self.value_cents
+            self.errors.add :base, "There is not balance available" 
+            raise ActiveRecord::RecordInvalid.new(self)
+          end 
+        end
       else
-        if account.balance_cents < self.value_cents
-          self.errors.add :base, "There is not balance available" 
-          raise ActiveRecord::RecordInvalid.new(self)
-        end 
+        if self.account_id_previously_changed? && self.value_cents_previously_changed? 
+          old_account = Account.find(self.previous_changes[:account_id][0])
+          new_account = Account.find(self.previous_changes[:account_id][1])
+
+          old_account.balance_cents += self.previous_changes[:value_cents][0]
+          old_account.update(balance_cents: old_account.balance_cents)
+
+          if new_account.balance_cents >= self.previous_changes[:value_cents][1]
+            new_account.balance_cents -= self.previous_changes[:value_cents][1]
+            new_account.update(balance_cents: new_account.balance_cents) 
+          else
+            if account.balance_cents < self.value_cents
+              self.errors.add :base, "There is not balance available" 
+              raise ActiveRecord::RecordInvalid.new(self)
+            end
+          end
+        else
+          if self.account_id_previously_changed?
+            old_account = Account.find(self.previous_changes[:account_id][0])
+            new_account = Account.find(self.previous_changes[:account_id][1])
+
+            old_account.balance_cents += self.value_cents
+            old_account.update(balance_cents: old_account.balance_cents)
+
+            if new_account.balance_cents >= self.value_cents
+              new_account.balance_cents -= self.value_cents
+              new_account.update(balance_cents: new_account.balance_cents)
+            else
+              self.errors.add :base, "There is not balance available" 
+              raise ActiveRecord::RecordInvalid.new(self)
+            end
+          end
+        end
       end
     end
   end
