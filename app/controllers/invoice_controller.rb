57e56f0ae61f288656521876964bed18021b6755
Matheus Henrique <matheussilva770@gmail.com>
2020-01-20 17:24:34 -0300

    fix query where search by closing day and add total view invoice

--- app/controllers/invoice_controller.rb
+++ app/controllers/invoice_controller.rb
@@ -5,8 +5,8 @@ class InvoiceController < ApplicationController
   def index
   	if params[:date].present? && params[:card_id].present?
   	  card = Card.find(params[:card_id])
-  	  end_date = (params[:date].to_date.beginning_of_month + card.closing_day.days) - 2.days
-  	  initial_date = end_date - 1.month
+  	  end_date = params[:date].to_date - 1.day
+      initial_date = end_date - 30.days
   	  @expense_cards = ExpenseCard.joins(:card).where(user_id: current_user).where(
   	  	"expense_cards.invoice_date between ? and ?", initial_date, end_date)
   	end 
--- app/helpers/invoice_helper.rb
+++ app/helpers/invoice_helper.rb
@@ -1,2 +1,11 @@
 module InvoiceHelper
+  def sum_invoice(expenses)
+  	sum = 0
+  	unless expenses.nil?
+  	  expenses.each do |expense|
+  	    sum += expense.value
+  	  end
+  	end
+  	sum
+  end
 end
